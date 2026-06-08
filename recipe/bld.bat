8c4eb039ce80f4da89e762fdaa53b8fe0c5476e1
H. Vetinari <h.vetinari@gmx.com>
2026-06-08 13:37:32 +1100

    use MESON_ARGS also on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,23 +1,14 @@
 @ECHO ON
 
-:: set pkg-config path so that host deps can be found
-:: (set as env var so it's used by both meson and during build with g-ir-scanner)
-set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%BUILD_PREFIX%\Library\lib\pkgconfig"
-
-:: get mixed path (forward slash) form of prefix so host prefix replacement works
-set "LIBRARY_PREFIX_M=%LIBRARY_PREFIX:\=/%"
-
 :: By default Meson tries to run glib-mkenums with the %BUILD_PREFIX% Python, which fails.
 :: In order to override this, we need to use a Meson machine file, because otherwise
 :: Meson prioritizes the results from the glib-2.0 pkg-config file, which don't work.
 echo [binaries] >native_file.txt
 echo glib-mkenums = ['%PREFIX%\python.exe', '%LIBRARY_PREFIX%\bin\glib-mkenums'] >>native_file.txt
 
-%BUILD_PREFIX%\Scripts\meson setup builddir ^
+meson setup builddir %MESON_ARGS% ^
     --backend=ninja ^
-    --buildtype=release ^
     --native-file=native_file.txt ^
-    --prefix=%LIBRARY_PREFIX_M% ^
     --wrap-mode=nofallback ^
     -Dbenchmark=disabled ^
     -Dcairo=enabled ^
