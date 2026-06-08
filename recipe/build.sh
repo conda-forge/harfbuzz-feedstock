ba6052e49fa695ec24b9e382bb4f152324ee336d
H. Vetinari <h.vetinari@gmx.com>
2026-06-08 12:33:50 +1100

    also unset CXXFLAGS for native-build (due to incompatible `-mtune`)

--- recipe/build.sh
+++ recipe/build.sh
@@ -49,6 +49,7 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
     # Unset them as we're ok with builds that are either slow or non-portable
     unset CFLAGS
     unset CPPFLAGS
+    unset CXXFLAGS
     export host_alias=$build_alias
 
     meson setup native-build \
